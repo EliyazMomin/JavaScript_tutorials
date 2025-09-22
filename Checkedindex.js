314123cc5cdec5a4e0c609183259abcd591100e8
Eliyaz <mominmdeliyaz@gmail.com>
2025-09-23 03:15:53 +0530

    Checked function added 1.1

--- Checkedindex.js
+++ Checkedindex.js
@@ -7,12 +7,22 @@ const mysubmit = document.getElementById("mysubmit");
 const subResult = document.getElementById("subResult");
 const modelresult = document.getElementById("modelresult");
 
-mysubmit.onclick= function(){
-    if(myCheckBox.ariaChecked){
-        subResult.textContent= 'You are subscribed!';
-    }else{
-        subResult.textContent= 'You are Not subscribed!';
+mysubmit.onclick = function() { 
+    if (myCheckBox.checked) {
+        subResult.textContent = 'You are subscribed!';
+    } else {
+        subResult.textContent = 'You are Not subscribed!';
     }
 
-   // if(){}
+    if(IphoneProMax.checked){
+        modelresult.textContent = 'You have selected Iphone Pro Max';
+    }else if(IphonePro){
+        modelresult.textContent = 'You have selected Iphone Pro';
+    }else if(IphonePlus){
+        modelresult.textContent = 'You have selected Iphone Plus';
+    }else if(Iphone){
+        modelresult.textContent = 'You have selected Iphone';
+    }
+
+
 }
